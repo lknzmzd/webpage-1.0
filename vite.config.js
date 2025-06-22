d8116fb4909d9bc8117ea3e9e599aecae5c5df37
Ilkin Azimzade <ilkin.azimzade.98@mail.ru>
2025-06-22 06:07:04 +0200

    Move build files to root

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
   base: '/webpage-1.0/',
+  plugins: [react()],
 })
